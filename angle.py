476f6b9cd3baa8562b25dcf3a3772070ed89efa0
talhabasit <talha.basit@gmail.com>
2022-11-06 16:49:47 +0100

    minor updates

---
+++ angle.py
@@ -0,0 +1,8 @@
+import numpy as np
+def rotation_from_two_vectors(a,b):
+    v = np.cross(a, b)
+    s = np.linalg.norm(v)
+    c = np.dot(a, b)
+    vx = np.array([[0, -v[2], v[1]], [v[2], 0, -v[0]], [-v[1], v[0], 0]])
+    r = np.eye(3) + vx + np.dot(vx,vx)*(1-c)/(s**2) 
+    return r
